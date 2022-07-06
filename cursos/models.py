805955b698b2e7994aba0a6793be7fcc5ef2771b
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-07-06 17:17:26 -0300

    CSS com Django

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=128)
+    descrição = models.TextField()
+    autor = models.CharField(max_length=128, default="EU", null=True)
+    ativo = models.BooleanField(default=True)
